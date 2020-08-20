4069fa17cfe60051dd47a178e35a409aa6f0ce09
samhabineza <hbnzsml@gmail.com>
2020-08-20 04:55:28 -0600

    add change

---
+++ JS/sign-up.js
@@ -0,0 +1,6 @@
+var form = document.getElementById("form").Value;
+var fName = document.getElementById("fname").Value;
+var lName = document.getElementById("lname").Value;
+var email = document.getElementById("email").Value;
+var password = document.getElementById("password").Value;
+var cPassword = document.getElementById("cpassword").Value;
---
+++ JS/uduclever.js
@@ -0,0 +1,6 @@
+var searchIcon = document.querySelector(".search-icon");
+var searchContainer = document.querySelector(".search-container");
+
+searchIcon.addEventListener("click", function () {
+  searchContainer.classList.toggle("display");
+});
