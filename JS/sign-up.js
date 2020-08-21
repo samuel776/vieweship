a7395afca8ba3ed4aceefc2903896755622e09c8
samhabineza <hbnzsml@gmail.com>
2020-08-21 12:21:31 -0600

    add changes

--- JS/sign-up.js
+++ JS/sign-up.js
@@ -1,20 +1,19 @@
-function validInput(){
+function validInput() {
+  var form = document.getElementById("form");
+  var fName = document.getElementById("fname").Value.trim();
+  var lName = document.getElementById("lname").Value.trim();
+  var email = document.getElementById("email").Value.trim();
+  var password = document.getElementById("password").Value.trim();
+  var cPassword = document.getElementById("cpassword").Value.trim();
 
-var form = document.getElementById("form");
-var fName = document.getElementById("fname").Value.trim();
-var lName = document.getElementById("lname").Value.trim();
-var email = document.getElementById("email").Value.trim();
-var password = document.getElementById("password").Value.trim();
-var cPassword = document.getElementById("cpassword").Value.trim();
-
-if(fname === ""){
+  if (fname === "") {
     setErrorFor(fname, " First name can not be empty");
-} else{
+  } else {
     setSuccessFor(fname);
-}
+  }
 
-form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
     validInput();
- }
+  });
 }
